ccd330ae88a21b0f50207eb3c2237d80f6b2e3e6
hahuang4 <142834501+hahuang4@users.noreply.github.com>
2024-04-29 22:44:44 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -16,7 +16,7 @@ public class APCalendar
 int count=0;
     for(int y=year1;y<=year2;y++)
       {
-        if(isLeapYear(y0)
+        if(isLeapYear(y)
            { 
           count++;
            }
